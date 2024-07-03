c3a6d06cef713b5b39adfcc0ca0fd0ae4b486a7b
Yihsiang Liow <yliow@ccis.edu>
2024-07-03 13:53:07 -0400

    no msg

---
+++ tests/main.py
@@ -0,0 +1,2 @@
+r:
+	cd python && mae
--- tests/python/main.py
+++ tests/python/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+try:
+    print(">>>> test imports ...")
+    import latextool_basic
+    import latexcircuit
+    import makemake
+    import findgrep
+except Exception as e:
+    print(">>>> ERROR")
+    print(e)
+
+
