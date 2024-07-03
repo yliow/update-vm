24b4255c966deab8674d681500f0202e0edc3113
Yihsiang Liow <yliow@ccis.edu>
2024-07-03 01:52:43 -0400

    no msg

---
+++ tests/opengl/test0/main.cpp
@@ -0,0 +1,17 @@
+#include <GL/freeglut.h>
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+}
+int main(int argc, char ** argv)
+{
+    glutInit(&argc, argv);
+    glutInitWindowPosition(300, 100);
+    glutInitWindowSize(400, 400);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutCreateWindow("OpenGL!!!");
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
