66a2c7f243ff782493e7fdd5fc4082a8d0398977
Yihsiang Liow <yliow@ccis.edu>
2024-07-03 15:25:46 -0400

    no msg

--- tests/python/main.py
+++ tests/python/main.py
@@ -1,9 +1,12 @@
+import os
+print("testing python ...")
 try:
-    print(">>>> test imports ...")
     import latextool_basic
     import latexcircuit
     import makemake
     import findgrep
+    os.system('findgrep')
+    os.system('makemake')
 except Exception as e:
     print(">>>> ERROR")
     print(e)
